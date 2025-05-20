21115ba9879f52ce179b29f28950b16e784c4e2b
lady.program <nymeriastark74@gmail.com>
2025-05-20 10:26:08 +0200

    Initial Commit

---
+++ iOS-SuperheroTests/iOS_SuperheroTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_SuperheroTests.swift
+//  iOS-SuperheroTests
+//
+//  Created by Mañanas on 20/5/25.
+//
+
+import Testing
+@testable import iOS_Superhero
+
+struct iOS_SuperheroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
